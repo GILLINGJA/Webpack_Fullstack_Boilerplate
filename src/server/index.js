c91c166c1cabf72c6b72ba1fce3fc3b66c652438
Josh <gillingja@gmail.com>
2019-09-07 23:52:54 +1000

    Set up CSP header

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const os = require('os');
+const csp = require('express-csp-header');
 
 const app = express();
+
+// setup of Content Security Policy (CSP) header
+app.use(csp({
+  policies: {
+    'default-src': [csp.NONE],
+    'script-src': [csp.SELF],
+    'style-src': [csp.SELF],
+    'img-src': [csp.SELF],
+    'form-action': [csp.SELF],
+    'block-all-mixed-content': true
+  }
+}));
 
 app.use(express.static('dist'));
 
